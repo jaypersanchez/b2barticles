fbe7d7b13aac1ead407125f290fb6ef8f377a53f
jaypersanchez <jaypersanchez@gmail.com>
2022-12-01 08:13:11 +0800

    initial save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
+import React, {useEffect, useState} from 'react';
+import { Button, Tabs, Tab, Container, Nav, Navbar, Form, Modal, ModalDialog } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Web3 from 'web3';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [currentAccount, setAccount] = useState();
+  const [currentAccountBalance, setAccountBalance] = useState();
+  const [networkId, setNetworkId] = useState();
+
+  const loadWeb3 = async() => {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      window.ethereum.enable();
+    }
+    else if(window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Please install metamask')
+    }
+  }
+
+  const loadWalletData = async() => {
+    const web3 = window.web3
+    let _networkid = await web3.eth.net.getId();
+    setNetworkId(_networkid)
+    const account = await web3.eth.getAccounts();
+    setAccount(account[0]);
+    let _balance = await web3.eth.getBalance(currentAccount)
+    //convert balance from wei to ether
+    let balance = await web3.utils.fromWei(_balance, "ether");
+    setAccountBalance(balance);
+  }
+
+  useEffect(() => {
+    //setup to connect to Metamask wallet or other wallet provider
+    loadWeb3();
+    //by calling getAccounts, we will know if we are connected to metamask
+    loadWalletData();
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand>Wallet Address: {currentAccount}</Navbar.Brand>
+            <Navbar.Brand>Wallet ETH Balance: {currentAccountBalance}</Navbar.Brand>
+          </Container>
+        </Navbar>
       </header>
+      <Tabs
+        defaultActiveKey="B2BSell"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="B2BSell" title="Articles for Sale">
+
+        </Tab>
+        <Tab eventKey="B2BBuy" title="Bought Articles">
+
+        </Tab>
+      </Tabs>
     </div>
   );
 }
